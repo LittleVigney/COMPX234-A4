6f9256e9164a307465739c09dfe95efe49d8e868
LittleVigney <1053813361@qq.com>
2025-06-07 17:47:37 +0800

    Client: download_file

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+import socket
+
 class Client:
     def __init__(self, _host, _port, _filelist):
         self.server_host = _host
@@ -5,12 +7,26 @@ class Client:
         self.filelistname = _filelist
         self.filelist = []
 
-    def start_client():
-        pass
+    def start_client(self):
+        # get names of files from txt file
+        self.get_filelist(self)
+
+        # download every file
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket:
+            # send request for every file name
+            pass
     
     def get_filelist(self):
         with open(self.filelistname, 'r') as f:
             for line in f:
                 if not line :
                     self.filelist.append(line)
-                    
+
+    def download_file(self, filename, client_socket):
+        dl_message = f"DOWNLOAD f{filename}"
+
+        # get reponse from server
+        
+
+    def send_rq(self, dl_message, client_socket):
+        
