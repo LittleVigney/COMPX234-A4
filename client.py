8796ca2b61202e3f0d377b3aaf8ab225ca8aa7f0
LittleVigney <1053813361@qq.com>
2025-06-07 17:32:41 +0800

    Client: get_filelist

--- client.py
+++ client.py
@@ -2,6 +2,15 @@ class Client:
     def __init__(self, _host, _port, _filelist):
         self.server_host = _host
         self.server_port = _port
-        self.filelist = _filelist
+        self.filelistname = _filelist
+        self.filelist = []
 
+    def start_client():
+        pass
     
+    def get_filelist(self):
+        with open(self.filelistname, 'r') as f:
+            for line in f:
+                if not line :
+                    self.filelist.append(line)
+                    
