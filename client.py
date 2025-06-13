5ebdd83a9e2e7182479d85903d37b7b730067f99
LittleVigney <1053813361@qq.com>
2025-06-13 22:39:35 +0800

    Client: download_file

--- client.py
+++ client.py
@@ -8,6 +8,7 @@ class Client:
         self.filelistname = _filelist
         self.filelist = []
         self.timeout = 1
+        self.retransmit_time = 5
 
     def start_client(self):
         # get names of files from txt file
@@ -25,13 +26,37 @@ class Client:
                     self.filelist.append(line)
 
     def download_file(self, filename, client_socket, server_addr):
-        dl_message = f"DOWNLOAD f{filename}"
+        dl_message = f"DOWNLOAD {filename}"
 
         # get reponse from server
-        
+        res = self.send_rq(dl_message, client_socket, server_addr)
+
+        # if not found
+        if res[0 : 4] == "ERR":
+            pass
+
+        # if file found
+        res_part = res.split()
+        file_size = res_part[res_part.index("SIZE") + 1]
+        port = res_part[res_part("PORT") + 1]
+
+        downloaded_file = ""
+        with open(downloaded_file, 'w') as f:
+            file_ct = 0
+
+            while file_ct <= file_size:
+                start_num = file_ct
+                end_num = start_num + 999
+
+                packet_message = f"FILE {filename} GET START {start_num} END {end_num}"
+                res_packet = self.send_rq(packet_message, client_socket, (self.server_host, port))
+
+                
+
+
 
     def send_rq(self, dl_message, client_socket, server_addr):
-        while True:
+        for retrans in range(self.retransmit_time):
             try:
                 client_socket.settimeout(self.timeout / 1000)
 
@@ -40,4 +65,6 @@ class Client:
 
                 return res.decode('utf-8')
             except socket.timeout:
-                
+                continue
+
+        
